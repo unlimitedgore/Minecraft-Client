7b3e5e8aadc1919f9a3d29a2199dc6f4723b964d
unlimitedgore <borgersnick@gmail.com>
2016-05-15 09:02:08 -0500

    Initial Commit

---
+++ client/setup.bat
@@ -0,0 +1,6 @@
+rmdir /s /q "C:\Users\nickb\Documents\Curse\Minecraft\Instances\FTB Infinity Evolved\mods"
+rmdir /s /q "C:\Users\nickb\Documents\Curse\Minecraft\Instances\FTB Infinity Evolved\config"
+mkdir "C:\Users\nickb\Documents\Curse\Minecraft\Instances\FTB Infinity Evolved\mods"
+mkdir "C:\Users\nickb\Documents\Curse\Minecraft\Instances\FTB Infinity Evolved\config"
+xcopy ".\mods" "C:\Users\nickb\Documents\Curse\Minecraft\Instances\FTB Infinity Evolved\mods" /s/h/e/k/f/c
+xcopy ".\config" "C:\Users\nickb\Documents\Curse\Minecraft\Instances\FTB Infinity Evolved\config" /s/h/e/k/f/c
